8582a38ec093f266836a628ac820b47745aed33b
Chevreau-Maxime <67205419+Chevreau-Maxime@users.noreply.github.com>
2020-06-21 12:57:28 +0200

    changed html for initial index

--- onConnection.js
+++ onConnection.js
@@ -1,5 +1,15 @@
 var name;
+
+var links = [["\"/Tetris/TetrisV2.html\"",                      "Tetris version 2"],
+             ["\"/Fractales/Fractal.html\"",                    "Fractals"],
+             ["\"/WebProject-Trajectory(MC.SS)/trajweb.html\"", "Projet Web Peip2"],
+             ["\"/Pathfinder/Pathfinder.html\"",                "Gridless Pathfinder"],
+             ["\"/WebGL-Smiley/index.html\"",                   "WebGL Smiley"]
+            ]
+
+
 function askName(){
+    spawnInterface();
     //Existing user ?
     name = localStorage.getItem("username");
     if (name == "null"){
@@ -12,3 +22,21 @@ function askName(){
         alert("Welcome back " + name + " !");
     }
 }
+
+
+
+
+
+function spawnInterface(){
+    console.log("start spawning html");
+    var container = document.getElementById("container");
+    container.innerHTML = "";
+
+    for (var i=0; i<links.length; i++){
+        var block = document.createElement("div");
+        var style = "\"position: absolute; width: 50%; left: 10%; top:"+ i * 8+"%;\"";
+        block.innerHTML = "<a style="+ style +" href="+ links[i][0] +"> "+ links[i][1] +" </a></br>";
+        container.appendChild(block);
+    }
+}
+
